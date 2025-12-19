c280654db45f7391896cc40f39267e78769ed0dc
Abhishek <abhishekY495.sec@gmail.com>
2025-12-19 18:49:59 +0530

    Refactor GET endpoint to return JSON metadata instead of image data

--- src/pages/[domain].ts
+++ src/pages/[domain].ts
@@ -9,16 +9,23 @@ export async function GET({
 
   const imageUrl = `https://cdn.prod.website-files.com/66c7823b4706f4a0a95d2d31/67b5064c2f792418120c4351_img-opengraph-missive.jpg`;
 
-  const imgRes = await fetch(imageUrl);
-  if (!imgRes.ok) {
-    return new Response("Not found", { status: 404 });
-  }
-  const imgData = await imgRes.blob();
-
-  return new Response(imgData, {
+  return new Response(JSON.stringify({ title: "Get Metadata" }), {
+    status: 200,
     headers: {
-      "Content-Type": "image/jpeg",
-      "Cache-Control": "public, max-age=86400",
+      "Content-Type": "application/json",
     },
   });
+
+  // const imgRes = await fetch(imageUrl);
+  // if (!imgRes.ok) {
+  //   return new Response("Not found", { status: 404 });
+  // }
+  // const imgData = await imgRes.blob();
+
+  // return new Response(imgData, {
+  //   headers: {
+  //     "Content-Type": "image/jpeg",
+  //     "Cache-Control": "public, max-age=86400",
+  //   },
+  // });
 }
