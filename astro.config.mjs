c984c8c92a7c5eb3ff154b14bcba91370f01a192
Abhishek <abhishekY495.sec@gmail.com>
2025-12-19 18:47:11 +0530

    Refactor project structure and integrate Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
+      enabled: true,
     },
 
-    imageService: "cloudflare"
-  })
+    imageService: "cloudflare",
+  }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
