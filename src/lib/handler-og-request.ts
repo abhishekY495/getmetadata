90f06e1b81ce637d090b07b9c78739d9c4b490c7
Abhishek <abhishekY495.sec@gmail.com>
2025-12-26 17:59:18 +0530

    chore: add logs for debugging

--- src/lib/handler-og-request.ts
+++ src/lib/handler-og-request.ts
@@ -22,8 +22,10 @@ export const handleOgRequest = async (
   try {
     if (fallback) {
       if (!isValidURL(fallback)) {
+        console.log("Fallback URL is invalid");
         return c.json({ status: "error", error: "Invalid fallback URL" }, 400);
       }
+      console.log("Fallback URL is valid");
     }
 
     const domainResponse = await fetchWithTimeout(domainUrl);
@@ -41,11 +43,14 @@ export const handleOgRequest = async (
     }
 
     if (!ogImage) {
+      console.log("No og image found");
       if (fallback) {
+        console.log("Using fallback URL");
         const fallbackResponse = await fetchWithTimeout(fallback);
         const contentType = fallbackResponse.headers.get("content-type") ?? "";
 
         if (!contentType.startsWith("image/")) {
+          console.log("Fallback URL is not an image");
           return c.json(
             {
               status: "error",
@@ -55,21 +60,29 @@ export const handleOgRequest = async (
           );
         }
 
+        console.log("Using fallback URL");
         const fallbackBuffer = await fallbackResponse.arrayBuffer();
         return c.body(fallbackBuffer, 200, {
           "Content-Type": contentType,
         });
       }
+
+      console.log("No fallback URL, using default og image");
       const defaultOgImage = await fetchDefaultOgImage();
       if (defaultOgImage) {
+        console.log("Default og image found");
         return c.body(defaultOgImage, 200, {
           "Content-Type": "image/png",
         });
       } else {
+        console.log(
+          "No default og image found, redirecting to default og image"
+        );
         return c.redirect(SITE_OG_IMAGE, 302);
       }
     }
 
+    console.log("Using og image");
     const ogImageResponse = await fetchWithTimeout(ogImage);
     const ogImageBuffer = await ogImageResponse.arrayBuffer();
     const contentType =
@@ -78,6 +91,7 @@ export const handleOgRequest = async (
       "Content-Type": contentType,
     });
   } catch (error) {
+    console.log("Error fetching og image");
     if ((error as Error).name === "AbortError") {
       return c.json(
         { status: "error", error: "Took too long to respond" },
@@ -86,10 +100,12 @@ export const handleOgRequest = async (
     }
 
     if (fallback) {
+      console.log("error - Using fallback URL");
       const fallbackResponse = await fetchWithTimeout(fallback);
       const contentType = fallbackResponse.headers.get("content-type") ?? "";
 
       if (!contentType.startsWith("image/")) {
+        console.log("error - Fallback URL is not an image");
         return c.json(
           {
             status: "error",
@@ -99,17 +115,23 @@ export const handleOgRequest = async (
         );
       }
 
+      console.log("error - Using fallback URL");
       const fallbackBuffer = await fallbackResponse.arrayBuffer();
       return c.body(fallbackBuffer, 200, {
         "Content-Type": contentType,
       });
     } else {
+      console.log("error - No fallback URL, using default og image");
       const defaultOgImage = await fetchDefaultOgImage();
       if (defaultOgImage) {
+        console.log("error - Default og image found");
         return c.body(defaultOgImage, 200, {
           "Content-Type": "image/png",
         });
       } else {
+        console.log(
+          "error - No default og image found, redirecting to default og image"
+        );
         return c.redirect(SITE_OG_IMAGE, 302);
       }
     }
