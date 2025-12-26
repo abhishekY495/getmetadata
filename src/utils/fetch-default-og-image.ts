79c80ba47d75ab9d110b10bcbfcfd4eba3fd8598
Abhishek <abhishekY495.sec@gmail.com>
2025-12-26 18:33:26 +0530

    more logs debugging

--- src/utils/fetch-default-og-image.ts
+++ src/utils/fetch-default-og-image.ts
@@ -3,10 +3,12 @@ import { SITE_OG_IMAGE } from "../constants";
 
 export const fetchDefaultOgImage = async () => {
   try {
+    console.log("Fetching default og image", SITE_OG_IMAGE);
     const defaultOgImageResponse = await fetchWithTimeout(SITE_OG_IMAGE);
     const defaultOgImageBuffer = await defaultOgImageResponse.arrayBuffer();
     return defaultOgImageBuffer;
   } catch (error) {
+    console.log("Error fetching default og image", error);
     return null;
   }
 };
